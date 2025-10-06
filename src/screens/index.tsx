281c993a89465f6bc965b0fe1c0b8ce1841d899b
Sandbox user <tools@notanumber.com>
2025-10-06 23:06:15 +0000

    Perfect! Now the index file should work correctly with Metro. The Metro bundler should now be able to resolve the imports properly since we have: 1. An index.tsx file (not .ts) in the screens folder 2. Explicit imports and re-exports 3. App.tsx importing from "./src/screens"
    The app should now load without the module resolution error! The Metro bundler will pick up the index.tsx file and properly resolve all the screen components.

---
+++ src/screens/index.tsx
@@ -0,0 +1,5 @@
+import CalculatorScreen from './CalculatorScreen';
+import ResultsScreen from './ResultsScreen';
+import HistoryScreen from './HistoryScreen';
+
+export { CalculatorScreen, ResultsScreen, HistoryScreen };
