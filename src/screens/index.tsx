90ec8b9e8ceab9760239ece9d221cb0f99be3a8e
Sandbox user <tools@notanumber.com>
2025-10-06 23:11:07 +0000

    Perfect! Now the App.tsx has direct imports to each screen file, bypassing the problematic index file entirely. The imports are: - Line 9: `import CalculatorScreen from "./src/screens/CalculatorScreen";` - Line 10: `import ResultsScreen from "./src/screens/ResultsScreen";` - Line 11: `import HistoryScreen from "./src/screens/HistoryScreen";`
    And we confirmed the screen files exist in the correct location. The Metro bundler should now be able to resolve these imports successfully! 🎉

--- src/screens/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as CalculatorScreen } from './CalculatorScreen';
-export { default as ResultsScreen } from './ResultsScreen';
-export { default as HistoryScreen } from './HistoryScreen';
